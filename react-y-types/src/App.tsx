cce1da30e548be6e78a484f716f12c9a10fb6396
jorgehara <empresamodelo2018@gmail.com>
2023-03-09 16:42:37 -0300

    pushInitial Pages

---
+++ react-y-types/src/App.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+import { RamdomFox } from './Components/RamdomFox/RamdomFox';
+
+
+
+
+
+export const App = (): JSX.Element => {
+
+
+
+  return (
+    <div className="App">
+      <h1>Home!</h1>
+      <h1>Home!</h1><h1>Home!</h1>
+              <RamdomFox image='' />
+            </div>
+  
+  )
+}
